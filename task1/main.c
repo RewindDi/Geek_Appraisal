ba24bf910a42a2d8765e146716ba03f197a24bdc
RewindDing <1431082586@qq.com>
2022-10-23 23:24:21 +0800

    My anwser

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+void Euler(double a)
+{
+	double n = 0.001;
+	while (1)
+	{
+	    double b = 0.999 * a;
+		printf("%lf      %lf\n", n, b);
+		if(a-b<0.000001)
+		{
+			break;
+		}
+		a = b; 
+		n += 0.001;
+	}
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+}
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	printf("       x       y\n");
+	Euler(a);
 	return 0;
 }
-
